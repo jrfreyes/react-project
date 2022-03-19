28a47e0a9e5c86848fe9fe6b54c8d8ee93214443
Herschel004 <61778826+Herschel004@users.noreply.github.com>
2022-03-19 18:40:02 +0800

    Add files via upload

---
+++ projects/ProjectList.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {Project} from './Project';
+import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
+
+function ProjectList({ projects, onSave }){
+    const [projectBeingEdited, setProjectBeingEdited] = useState({});
+    const handleEdit =(project) =>{
+        setProjectBeingEdited(project);
+    };
+    const cancelEditing = () => {
+        setProjectBeingEdited({});
+    };
+    return (
+        <div className = "row">
+        {projects.map(project => (
+        <div key={project.id} className="cols-sm">
+            {project === projectBeingEdited ? (
+                <ProjectForm
+                onSave = {onSave}
+                onCancel ={cancelEditing}
+                />
+            ) : (
+                <ProjectCard project={project} onEdit = {handleEdit}/>
+            )}
+        </div>
+    ))}
+     </div>
+    );
+}
+
+ProjectList.propTypes = {
+    projects:PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+    onSave: PropTypes.func.isRequired
+};
+
+export default ProjectList;
