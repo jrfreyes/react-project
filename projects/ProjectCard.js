7aa995687c5a10217b499840dc6574ff07a0022b
Herschel004 <61778826+Herschel004@users.noreply.github.com>
2022-04-05 19:02:26 +0800

    Update ProjectCard.js

--- projects/ProjectCard.js
+++ projects/ProjectCard.js
@@ -1,6 +1,7 @@
 import {Project} from './Project';
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 function formatDescription(description){
     return description.substring(0,60) + '...';
@@ -16,11 +17,13 @@ return(
     <div className="card">
         <img src={project.imageUrl} alt={project.name}/>
         <section className="section dark">
+            <Link to ={'/projects/'+project.id}>
             <h5 className="strong">
                 <strong>{project.name}</strong>
             </h5>
             <p>{formatDescription(project.description)}</p>
             <p>Budget: {project.budget.toLocaleString()}</p>
+            </Link>
             <button 
                 class="bordered"
                     onClick={()=>{
